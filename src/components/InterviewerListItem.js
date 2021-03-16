b260e7457e757dc4b79fc169f2b6d5cc3bf19f02
Amazigh-SR <s_Red009@hotmail.com>
2021-03-16 18:59:06 -0400

    Create an interviewerListItem functional component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./InterviewerListItem.scss";
+import classNames from "classnames";
+
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected:boolean - to determine if an interview is selected or not
+// setInterviewer:function - sets the interviewer upon selection
+
+const InterviewerListItem = function (props) {
+  const liClassNames = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li className={liClassNames} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
