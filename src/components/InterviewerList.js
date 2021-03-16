de8b9508e1af80255d4e1659fe0e5124afce5f19
Amazigh-SR <s_Red009@hotmail.com>
2021-03-16 18:57:05 -0400

    Create an interviewList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+// Our InterviewerList takes in three props:
+// interviewers:array - an array of objects containing the information of each interviewer
+// interviewer:number - the id of an interviewer
+// setInterviewer:function - a function that accepts an interviewer id
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
+];
+
+const InterviewerList = function (props) {
+  const interviewerList = interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={props.interviewer === interviewer.id}
+        setInterviewer={(event) => props.setInterviewer(interviewer.id)} // refactored this - i.e. id passed here instead down to the list item
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerList}</ul>
+    </section>
+  );
+};
+
+export default InterviewerList;
