e6c0ac87d02184962a212cbedd3fd7c8ede130d5
Amazigh-SR <s_Red009@hotmail.com>
2021-03-15 20:41:26 -0400

    Include the logic for the button classes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 import "components/Button.scss";
+// import { action } from "@storybook/addon-actions";
+import classNames from "classnames";
+// const classNames = require("classnames");
+
+// Base uses no props and is considered the default button
+// Confirm uses the confirm prop to apply the .button--confirm modifier class
+// Danger uses the danger prop to apply the .button--danger modifier class
+// Clickable uses the onClick prop to handle the button click event
+// Disabled uses the disabled prop to apply the disabled attribute to the button element
 
 export default function Button(props) {
-  return <></>;
+  let className = classNames(
+    "button",
+    { "button--confirm": props.confirm },
+    { "button--danger": props.danger }
+  );
+
+  return (
+    <button
+      className={className}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
