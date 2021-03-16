c92c679f96427703cc2bfe8a4a61dccb3bc12cfa
Amazigh-SR <s_Red009@hotmail.com>
2021-03-16 19:43:47 -0400

    Create index file & include Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+
+const Appointment = function () {
+  return <article className="appointment"></article>;
+};
+
+export default Appointment; //Do I need to include function?
