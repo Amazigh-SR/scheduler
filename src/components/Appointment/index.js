9073598d843e888dfebfe55aaa0a3f3fa64e811f
Amazigh-SR <s_Red009@hotmail.com>
2021-03-17 14:56:40 -0400

    Add the Header, Show and Empty components to index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 const Appointment = function (props) {
-  return <article className="appointment">{props.time}</article>;
+  // console.log("test__Mentor", props);
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 };
 
 export default Appointment;
