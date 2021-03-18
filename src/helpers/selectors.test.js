62047fe58307d9412cea47ca30258f1414c81adb
Amazigh-SR <s_Red009@hotmail.com>
2021-03-17 23:35:52 -0400

    Switched import method to require

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,4 +1,5 @@
-import { getAppointmentsForDay } from "helpers/selectors";
+// import { getAppointmentsForDay } from "helpers/selectors";
+const getAppointmentsForDay = require("./selectors");
 
 const state = {
   days: [
