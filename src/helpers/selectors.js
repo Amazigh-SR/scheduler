b0bfdf96fa65c19c4629d94362b7d7d5262b5b5c
Amazigh-SR <s_Red009@hotmail.com>
2021-03-17 23:36:07 -0400

    Implemented the selector function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,44 @@
+const state = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2, 3],
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [4, 5],
+    },
+  ],
+  appointments: {
+    1: { id: 1, time: "12pm", interview: null },
+    2: { id: 2, time: "1pm", interview: null },
+    3: {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Archie Cohen", interviewer: 2 },
+    },
+    4: { id: 4, time: "3pm", interview: null },
+    5: {
+      id: 5,
+      time: "4pm",
+      interview: { student: "Chad Takahashi", interviewer: 2 },
+    },
+  },
+};
+
+const getAppointmentsForDay = function (state, day) {
+  //Get appointments array for a given day
+  const appointmentsForDay = state.days.find((dayObj) => day === dayObj.name)
+    ? state.days
+        .find((dayObj) => day === dayObj.name)
+        .appointments.map((element) => state.appointments[element])
+    : [];
+
+  return appointmentsForDay;
+};
+
+module.exports = getAppointmentsForDay;
+// import default getAppointmentsForDay
+// console.log(getAppointmentsForDay(state, ""));
