f2a9dcc62891af1b928fe842410948e116c03e64
Amazigh-SR <s_Red009@hotmail.com>
2021-03-18 00:28:19 -0400

    Combine all states into one master object

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,12 +57,23 @@ const appointmentsRender = appointments.map((appointment) => {
 });
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  // const [appointments, setAppointments] = useState({});
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
+
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => {
+    setState((prev) => ({ ...prev, days }));
+  };
 
   useEffect(() => {
     axios.get("/api/days").then((response) => {
-      console.log(response.data);
       setDays([...response.data]);
     });
   }, []);
@@ -77,7 +88,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
