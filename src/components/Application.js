8c4318ad7f5e571c6e0108f9503703597488ec52
Amazigh-SR <s_Red009@hotmail.com>
2021-03-17 22:21:55 -0400

    Refactor application component to fetch days data instead of dummy data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
@@ -56,27 +56,16 @@ const appointmentsRender = appointments.map((appointment) => {
   return <Appointment key={appointment.id} {...appointment} />;
 });
 
-//Used as dummy data for the DayList component
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      console.log(response.data);
+      setDays([...response.data]);
+    });
+  }, []);
 
   return (
     <main className="layout">
