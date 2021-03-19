442048589a64b58f391fad678044f8d30d475ab2
Amazigh-SR <s_Red009@hotmail.com>
2021-03-18 22:19:56 -0400

    Implement different modes with useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,45 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVE = "SAVE";
+const CANCEL = "CANCEL";
 
 const Appointment = function (props) {
-  // console.log("test__Mentor", props);
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {/* {props.interview ? (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       ) : (
         <Empty />
+      )} */}
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]} //Empty array for the time being
+          onSave={() => transition(SAVE)}
+          onCancel={() => back()}
+        />
+      )}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
       )}
     </article>
   );
