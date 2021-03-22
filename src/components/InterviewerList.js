aa12badcd7d525bd1ac6f2677e6117b61abf972e
Amazigh-SR <s_Red009@hotmail.com>
2021-03-22 00:35:13 -0400

    Fix bug related to list interviewers for each day

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,26 +7,28 @@ import InterviewerListItem from "./InterviewerListItem";
 // interviewer:number - the id of an interviewer
 // setInterviewer:function - a function that accepts an interviewer id
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
-];
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
+// ];
 
 const InterviewerList = function (props) {
-  const interviewerList = interviewers.map((interviewer) => {
-    return (
-      <InterviewerListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={interviewer.id === props.value}
-        setInterviewer={(event) => props.onChange(interviewer.id)}
-      />
-    );
-  });
+  const interviewerList =
+    props.interviewers &&
+    props.interviewers.map((interviewer) => {
+      return (
+        <InterviewerListItem
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected={interviewer.id === props.value}
+          setInterviewer={(event) => props.onChange(interviewer.id)}
+        />
+      );
+    });
 
   return (
     <section className="interviewers">
