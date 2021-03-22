b7f79021251ac335b6111bd4613a0acc827ee35f
Amazigh-SR <s_Red009@hotmail.com>
2021-03-22 02:35:17 -0400

    Remove interviewers  dummy data

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,19 +1,12 @@
 import React from "react";
 import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
 // Our InterviewerList takes in three props:
 // interviewers:array - an array of objects containing the information of each interviewer
 // interviewer:number - the id of an interviewer
 // setInterviewer:function - a function that accepts an interviewer id
-
-// const interviewers = [
-//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
-// ];
 
 const InterviewerList = function (props) {
   const interviewerList =
@@ -36,6 +29,10 @@ const InterviewerList = function (props) {
       <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
 };
 
 export default InterviewerList;
