2014fb106faa7942ae6634767640f873e63420c0
Amazigh-SR <s_Red009@hotmail.com>
2021-03-24 15:53:44 -0400

    Add the delete appointment happy path test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -8,8 +8,6 @@ describe("Appointments", () => {
   it("should book an interview", () => {
     cy.get("[alt='Add']").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    //Decided to choose a different interviewer (instead of using Sylvia) to make the test less fragile
-    //Otherwise the query is not pointing at the same show card
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
@@ -25,5 +23,11 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
-  it("should cancel an interview", () => {});
+  it("should cancel an interview", () => {
+    cy.get("[alt='Delete']").first().click({ force: true });
+    cy.contains("Confirm").click();
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
 });
